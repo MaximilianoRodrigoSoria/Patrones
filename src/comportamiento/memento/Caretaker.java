cd5c88526f2e8b8d67ba28faa14f06a84f7ad1af
maximiliano.soria <maximiliano.soria@viverebrasil.local>
2019-12-10 13:40:58 -0300

    Memento

---
+++ src/comportamiento/memento/Caretaker.java
@@ -0,0 +1,14 @@
+package comportamiento.memento;
+
+public class Caretaker
+{
+     private Memento memento;
+     
+     public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+     
+     public Memento getMemento() {
+        return this.memento;
+    }
+}
