468b7748e8e55600b6cd6fef7d28147b2aca1466
maximiliano.soria <maximiliano.soria@viverebrasil.local>
2019-12-11 13:29:37 -0300

    Agrando comentario

--- src/creacion/factorymethod/Factory.java
+++ src/creacion/factorymethod/Factory.java
@@ -2,7 +2,7 @@ package creacion.factorymethod;
 
 public interface Factory {
 	
-	//Se declaran los tipos de auto a devolver
+	//Se declaran los tipos de auto a devolver con el nombre de las clases
 	public Auto getToyota(String marca, Double precio);
 	public Auto getBMW(String marca, Double precio);
 	public Auto getAudi(String marca, Double precio);
